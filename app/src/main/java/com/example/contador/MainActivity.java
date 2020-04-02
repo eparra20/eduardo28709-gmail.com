89015e0529703115f605274cf2ec4e5a9c956df3
Edu <eduardo@poincenot.com>
2020-04-02 20:48:28 -0300

    Se Agregaron atributos y findViewById

--- app/src/main/java/com/example/contador/MainActivity.java
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.contador;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonSumar;
+    private Button buttonRestar;
+    private TextView textViewContador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.buttonSumar = findViewById(R.id.buttonSumar);
+        this.buttonRestar = findViewById(R.id.buttonRestar);
+        this.textViewContador = findViewById(R.id.textViewContador);
     }
 }
